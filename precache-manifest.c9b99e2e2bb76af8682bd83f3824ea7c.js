05c3c9640dd287efe361f16e69cdb6bf2aa24f45
darshanaswani <darshan.aswani64@gmail.com>
2019-10-30 00:09:38 +0530

    Updates

---
+++ precache-manifest.c9b99e2e2bb76af8682bd83f3824ea7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d674dc2a078a49b4b10a342fb23d5ec",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "62e8ce76e1c681ba8c91",
+    "url": "/robofriends/static/css/main.8277e9b5.chunk.css"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/robofriends/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "62e8ce76e1c681ba8c91",
+    "url": "/robofriends/static/js/main.10c558cb.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
