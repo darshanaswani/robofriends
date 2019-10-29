3193b5f11d6a4fb261239ca8da8c81ac268696ae
darshanaswani <darshan.aswani64@gmail.com>
2019-10-29 23:58:55 +0530

    adding starter files

---
+++ build/precache-manifest.d0c6feaf503da44603391347f6791f7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc1e7b4466157717558e76f3557143f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7968dbcf3c0498f536e9",
+    "url": "/robofriends/static/css/main.8277e9b5.chunk.css"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/robofriends/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "7968dbcf3c0498f536e9",
+    "url": "/robofriends/static/js/main.ba6a69ae.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ src/components/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBox = ({ searchfield, searchChange }) => {
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="search robots"
+        className="pa3 ba b--green bg-lightest-blue"
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
+export default SearchBox;
